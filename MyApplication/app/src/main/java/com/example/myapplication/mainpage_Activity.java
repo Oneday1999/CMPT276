9a7439ae30660223e0b812febc8f30c4c9886742
XYP9XXX <99450192+XYP9XXX@users.noreply.github.com>
2023-10-25 19:04:39 -0700

    fix some error

--- MyApplication/app/src/main/java/com/example/myapplication/mainpage_Activity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/mainpage_Activity.java
@@ -1,14 +1,13 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class mainpage_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
     }
 }
