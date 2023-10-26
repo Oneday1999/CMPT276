5b5d8a5a219eced6ad0b76359bcd14dc934216a0
XYP9XXX <99450192+XYP9XXX@users.noreply.github.com>
2023-10-25 19:24:07 -0700

    back up for main branch

---
+++ MyApplication/app/src/main/java/com/example/myapplication/mainpage_Activity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mainpage_Activity extends AppCompatActivity {
+    @Override
+    // Test
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+}
