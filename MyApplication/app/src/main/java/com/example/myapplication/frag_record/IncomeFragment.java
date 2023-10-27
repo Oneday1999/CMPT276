4d7ba73a0efd3cad7e746e891acb08e754fd60ba
XYP9XXX <99450192+XYP9XXX@users.noreply.github.com>
2023-10-26 17:48:46 -0700

    解决record界面的tablayout颜色问题

--- MyApplication/app/src/main/java/com/example/myapplication/frag_record/IncomeFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/frag_record/IncomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.frag_record;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import com.example.myapplication.R;
 
